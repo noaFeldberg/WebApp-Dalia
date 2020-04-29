18d4e16940a6ac54f351058cf2584511cd1a9786
dror <drorwb@gmail.com>
2020-04-29 11:38:25 +0300

    Improve last version
    *Make the title in na-bar leed to homepage
    *Cleaner code at Projects.jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,47 +1,35 @@
 import React from "react";
-import {Bootstrap, Grid, Row, Col} from 'react-bootstrap'
+import { Bootstrap, Grid, Row, Col } from 'react-bootstrap'
 import ProjectCard from "./ProjectCard";
 import cards from "../cards";
 
 //*********************** Project function *******************************
-function Project(){
-    return(
-      <div>
+let cardsToShow = (
+  <Col className="cardsCol">
+    {cards.map((card, index) => {
+      return <ProjectCard
+          id={card.id}
+          Img={card.Img}
+          title={card.title}>
+        </ProjectCard>
+    })}
+  </Col>
+);
+
+function Project() {
+  return (
+    <div>
       <div className="headeings" id="projectHeader"><h1>Projects</h1></div>
-      
+
       <Row>
-        <Col className="cardsCol" >
-        <ProjectCard id={cards[0].id} Img={cards[0].Img} title={cards[0].title}/>
-        <ProjectCard id={cards[1].id} Img={cards[1].Img} title={cards[1].title}/>
-        <ProjectCard id={cards[2].id} Img={cards[2].Img} title={cards[2].title}/>
-        <ProjectCard id={cards[3].id} Img={cards[3].Img}/>
-        </Col>
-        <Col className="cardsCol">
-        <ProjectCard Img={cards[0].Img}/>
-        <ProjectCard Img={cards[1].Img}/>
-        <ProjectCard Img={cards[2].Img}/>
-        <ProjectCard Img={cards[3].Img}/>
-        </Col>
-        <Col className="cardsCol">
-        <ProjectCard Img={cards[0].Img}/>
-        <ProjectCard Img={cards[1].Img}/>
-        <ProjectCard Img={cards[2].Img}/>
-        <ProjectCard Img={cards[3].Img}/>
-        </Col>
-        <Col className="cardsCol">
-        <ProjectCard Img={cards[0].Img}/>
-        <ProjectCard Img={cards[1].Img}/>
-        <ProjectCard Img={cards[2].Img}/>
-        <ProjectCard Img={cards[3].Img}/>
-        </Col>
-        <Col className="cardsCol">
-        <ProjectCard Img={cards[0].Img}/>
-        <ProjectCard Img={cards[1].Img}/>
-        </Col>
-        </Row>
-    
-        </div>
-    );
+        {cardsToShow}
+        {cardsToShow}
+        {cardsToShow}
+        {cardsToShow}
+      </Row>
+
+    </div>
+  );
 }
 
 export default Project;

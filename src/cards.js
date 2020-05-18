8d2a77263f38461890feb790f53a05944212cf55
noafeld <noa7feld@gmail.com>
2020-05-18 23:32:25 +0300

    arrangr images

--- src/cards.js
+++ src/cards.js
@@ -1,7 +1,7 @@
-var img3 = require('./images/img3.jpg')
+var img6 = require('./images/img6.jpg')
 var img4 = require('./images/img4.jpg')
 var img7 = require('./images/img7.jpg')
-var img8 = require('./images/img8.jpg')
+var img2 = require('./images/img2.jpg')
 
 const cards = [
     {
@@ -12,32 +12,12 @@ const cards = [
     {
       key: 2,
       title: "Project2",
-      Img: img8
+      Img: img6
     },
     {
       key: 3,
       title: "Project3",
-      Img: img3
-    },
-    {
-      key: 4,
-      title: "Project4",
-      Img: img7
-    },
-    {
-      key: 1,
-      title: "Project1",
-      Img: img4
-    },
-    {
-      key: 2,
-      title: "Project2",
-      Img: img8
-    },
-    {
-      key: 3,
-      title: "Project3",
-      Img: img3
+      Img: img2
     },
     {
       key: 4,
